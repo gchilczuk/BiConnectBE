e4df7a38a39126541910fda939c6e79b930dd0f8
Grzegorz Chilczuk <chilczukg@gmail.com>
2018-05-22 22:54:41 +0200

    use ugly update to fix counting members of meeting

--- descriptor/views.py
+++ descriptor/views.py
@@ -54,6 +54,7 @@ class MeetingViewSet(DetailSerializerMixin, ModelViewSet):
     serializer_detail_class = MeetingDetailSerializer
 
     def get_queryset(self, *args, **kwargs):
+        for m in Meeting.objects.all(): m.save()
         return Meeting.objects.filter(group=self.kwargs['parent_lookup_group'])
 
     def create(self, request, *args, **kwargs):

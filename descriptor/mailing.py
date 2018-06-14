8eb515006af3807e5990ffe3de3f0e82bd88935c
Grzegorz Chilczuk <chilczukg@gmail.com>
2018-06-14 19:40:52 +0200

    add html message

--- descriptor/mailing.py
+++ descriptor/mailing.py
@@ -4,17 +4,29 @@ from BiConnect import settings
 
 
 def prepare_mail_body(speech):
-    return f'Twoja wypowiedź: \n\n {str(speech)}' \
-           f'Jeśli masz jakieś uwagi edytuj swoją wypowiedź tutaj' \
-           f'--tutaj link--'
+    address = 'http://www.google.pl'
+    return f'Na spotkaniu w dniu {speech.date} została odnotowana twoja wypowiedź: \n\n {str(speech)} \n\n' \
+           f'Swoją wypowiedź możesz potwierdzić lub edytować jeśli masz jakieś uwagi pod adresem: {address} \n' \
+           f'Jeżeli nie potwierdzisz wypowiedzi zostanie ona automatycznie potwierdzona po tygodniu od spotkania.'
+
+
+def prepare_mail_html_body(speech):
+    address = 'http://www.google.pl'
+    html_speech = str(speech).replace('\n', '<br/>')
+    return f'Na spotkaniu w dniu {speech.date} została odnotowana twoja wypowiedź: <p> {html_speech} </p>' \
+           f'<p>Swoją wypowiedź możesz potwierdzić lub edytować jeśli masz jakieś uwagi: ' \
+           f'<a href="{address}">tutaj</a>.<br/>' \
+           f'Jeżeli nie potwierdzisz wypowiedzi zostanie ona automatycznie potwierdzona po tygodniu od spotkania.</p>'
+
 
 
 def send_speechsum_mail(speech):
     if speech.person.email:
         return send_mail(
-            'Podsumowanie wypowiedzi',
-            prepare_mail_body(speech),
-            settings.EMAIL_HOST_USER,
-            [speech.person.email],
+            subject='Podsumowanie wypowiedzi',
+            message=prepare_mail_body(speech),
+            html_message=prepare_mail_html_body(speech),
+            from_email=settings.EMAIL_HOST_USER,
+            recipient_list=[speech.person.email],
             fail_silently=False,
         )

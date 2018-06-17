0a312f5662bd2c1ced1df58f43358b5e5a08f1e3
marwaw <martyna.waw96@gmail.com>
2018-06-17 11:41:25 +0200

    prepare address

--- descriptor/mailing.py
+++ descriptor/mailing.py
@@ -3,21 +3,24 @@ from django.core.mail import send_mail
 from BiConnect import settings
 
 
+def prepare_address(speech):
+    return f'http://localhost:3000/?m={speech.meeting_id}&s={speech.id}'
+
+
 def prepare_mail_body(speech):
-    address = 'http://www.google.pl'
+    address = prepare_address(speech)
     return f'Na spotkaniu w dniu {speech.date} została odnotowana twoja wypowiedź: \n\n {str(speech)} \n\n' \
            f'Swoją wypowiedź możesz potwierdzić lub edytować jeśli masz jakieś uwagi pod adresem: {address} \n' \
            f'Jeżeli nie potwierdzisz wypowiedzi zostanie ona automatycznie potwierdzona po tygodniu od spotkania.'
 
 
 def prepare_mail_html_body(speech):
-    address = 'http://www.google.pl'
+    address = prepare_address(speech)
     html_speech = str(speech).replace('\n', '<br/>')
     return f'Na spotkaniu w dniu {speech.date} została odnotowana twoja wypowiedź: <p> {html_speech} </p>' \
            f'<p>Swoją wypowiedź możesz potwierdzić lub edytować jeśli masz jakieś uwagi: ' \
            f'<a href="{address}">tutaj</a>.<br/>' \
            f'Jeżeli nie potwierdzisz wypowiedzi zostanie ona automatycznie potwierdzona po tygodniu od spotkania.</p>'
-
 
 
 def send_speechsum_mail(speech):
